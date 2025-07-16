85867df6064b6790477b5bcc154157333dd09dac
Kester Simm <68278724+kesi03@users.noreply.github.com>
2025-07-16 04:32:39 +0000

    added

---
+++ plugins/pipe-dream-maven/src/main/java/com/mockholm/tools/mojos/RunBuild.java
@@ -0,0 +1,16 @@
+package com.mockholm;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
+
+
+public class RunBuild extends AbstractMojo{
+
+    public void execute() {
+
+    }
+}
